63cfcbb9028c9314fa62d6e7be0499f50f4e3cd6
caiohbe <caiohbe@gmail.com>
2022-10-09 09:51:17 -0300

    Header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function Header({setSeatNumbers}) {
     return (
         <Link to={'/'}>
-            <Top onClick={() => setSeatNumbers([])}>CINEFLIX</Top>
+            <Top onClick={() => setSeatNumbers([])}>CINEFLEX</Top>
         </Link>
         
     )
